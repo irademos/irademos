13029b1ee421e71bddb452aba11a5740aa5cca3d
Chris <Christopher@aphid.io>
2020-11-14 16:01:37 -0500

    removed django-heroku from settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,6 +121,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
-
-django_heroku.settings(locals())
-# just adding this comment so that i can build again
